a2341a09d9aa31a58b318b783342b90f869b7fac
Glinkowski Damian <d.glinkowski@tme.pl>
2017-03-27 08:53:44 +0200

    Clean algo; test script

--- match_products.php
+++ match_products.php
@@ -1,103 +1,100 @@
 <?php
 
-function search($index, $products, $query) 
+$result = [];
+$products = json_decode(file_get_contents($argv[1]), true);
+$rule = json_decode(file_get_contents($argv[2]), true);
+
+$index = [];
+foreach ($products as $product) {
+    foreach ($product['parameters'] as $name => $value) {
+        $index[$name] ?? $index[$name] = ['all_ids' => []];
+        $index[$name][$value] ?? $index[$name][$value] = [];
+
+        $index[$name][$value][$product['id']] = $product['id'];
+        $index[$name]['all_ids'][$product['id']] = $product['id'];
+    }
+}
+
+$findProducts = get_find_products($rule['findProducts']);
+
+$findSearchResult = search($index, $products, $findProducts);
+foreach ($findSearchResult as $baseProductId) {
+    $symbolKey = $products[$baseProductId]['symbol'];
+    $result[$symbolKey] = [];
+
+    $matchProducts = get_match_products($rule['matchProducts'], $products, $baseProductId);
+
+    $matchSearchResult = search($index, $products, $matchProducts);
+    foreach ($matchSearchResult as $entry) {
+        $result[$symbolKey][] = $products[$entry]['symbol'];
+    }
+}
+
+file_put_contents('result.json', json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+
+/**
+ * @param array $criteria
+ *
+ * @return \Generator
+ */
+function get_find_products(array $criteria): \Generator
+{
+    foreach ($criteria as $criterion) {
+        yield $criterion['parameter'] => $criterion['equals'];
+    }
+}
+
+/**
+ * @param array $criteria
+ * @param array $products
+ * @param string $productId
+ *
+ * @return \Generator
+ */
+function get_match_products(array $criteria, array $products, string $productId): \Generator
+{
+    foreach ($criteria as $criterion) {
+        yield $criterion['parameter'] => $criterion['equals'] === 'this' ?
+            $products[$productId]['parameters'][$criterion['parameter']] :
+            $criterion['equals'];
+    }
+}
+
+/**
+ * @param array $index
+ * @param array $products
+ * @param \Generator $criteria
+ *
+ * @return array
+ */
+function search(array $index, array $products, \Generator $criteria): array
 {
     $results = [];
-    $isEmpties = [];
+    $emptyParameters = [];
 
-    foreach ($query as $parameter => $value) {
+    foreach ($criteria as $parameter => $value) {
         switch ($value) {
             case 'any':
                 $results[] = $index[$parameter]['all_ids'];
                 break;
             case 'is empty':
-                $isEmpties[$parameter] = $parameter;
+                $emptyParameters[$parameter] = $parameter;
                 break;
             default:
                 $results[] = $index[$parameter][$value];
         }
     }
 
-    $finalResult = null;
+    $searchResult = count($results) === 1 ?
+        $results[0] :
+        call_user_func_array('array_intersect_key', $results);
 
-    if (count($results) == 1) {
-        $finalResult = $results[0];
-    } else {
-        $finalResult = call_user_func_array('array_intersect_key', $results);
-    }
-    
-    $finalResult = array_flip($finalResult);
-
-    if (count($isEmpties) > 0) {
-        $finalResult = array_filter($finalResult, function ($entry) use ($products, $isEmpties) {
-            $interseciton = array_intersect_key($products[$entry]['parameters'], $isEmpties);
-            return count($interseciton) == 0;
+    if (!empty($emptyParameters)) {
+        $searchResult = array_filter($searchResult, function (string $entry) use ($products, $emptyParameters): bool {
+            $interseciton = array_intersect_key($products[$entry]['parameters'], $emptyParameters);
+            return empty($interseciton);
         });
     }
 
-    return $finalResult;
+    return $searchResult;
 }
-
-$jsonData = file_get_contents("products.json");
-$products = json_decode($jsonData, true);
-$allIds = array_keys($products);
-$index = [];
-
-foreach ($products as $product) {
-    foreach ($product['parameters'] as $parameterName => $parameterValue) {
-        if (!array_key_exists($parameterName, $index)) {
-            $index[$parameterName] = ['all_ids' => []];
-        }
-        if (!array_key_exists($parameterValue, $index[$parameterName])) {
-            $index[$parameterName][$parameterValue] = [];
-        }
-        $index[$parameterName][$parameterValue][$product['id']] = $product['id'];
-        $index[$parameterName]['all_ids'][$product['id']] = $product['id'];
-    }
-}
-
-$totalTime = microtime(true);
-$rule = json_decode(file_get_contents($argv[1]), true);
-
-$findProducts = [];
-
-foreach ($rule['findProducts'] as $criterion) {
-    $findProducts[$criterion['parameter']] = $criterion['equals'];
-}
-
-$result = search($index, $products, $findProducts);
-
-$jsonFinalResult = [];
-
-foreach ($result as $baseProductId) {
-    $symbolKey = $products[$baseProductId]['symbol'];
-    $jsonFinalResult[$symbolKey] = [];
-    $time = microtime(true);
-    $matchProducts = [];
-
-    foreach ($rule['matchProducts'] as $criterion) {
-        switch ($criterion['equals']) {
-            case 'this':
-                $matchProducts[$criterion['parameter']] = $products[$baseProductId]['parameters'][$criterion['parameter']];
-                break;
-            default:
-                $matchProducts[$criterion['parameter']] = $criterion['equals'];
-        }
-    }
-
-    $searchTime = microtime(true);
-    $matchSearchResult = search($index, $products, $matchProducts);
-    $searchTime = microtime(true) - $searchTime;
-    $time = microtime(true) - $time;
-
-    foreach ($matchSearchResult as $entry) {
-        $jsonFinalResult[$symbolKey][] = $products[$entry]['symbol'];
-    }
-}
-
-$totalTime = microtime(true) - $totalTime;
-
-echo "TOTAL TIME: " . $totalTime . "\n";
-echo "RESULT COUNT: " . count($result) . "\n";
-
-file_put_contents('result.json', json_encode($jsonFinalResult, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
