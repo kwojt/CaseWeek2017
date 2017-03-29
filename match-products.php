e9f333849bd12e751e92bf9f9fb8ed4e817697c0
Krzysztof Wójt <kwojt@g.pl>
2017-03-29 11:19:28 +0200

    Basics

--- match-products.php
+++ match-products.php
@@ -1,1 +1,11 @@
+<?php
 // Główna paczka
+$time = microtime(true);
+
+$rule = json_decode( file_get_contents( $argv[1]), true);
+$base = json_decode( file_get_contents( $argv[2]), true);
+
+
+$time = microtime(true) - $time;
+echo "\nTime used: " . $time . "\n";
+echo "\n";
